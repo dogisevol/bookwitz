a89782ad6e20ddf33f841a1eb00eebfe4a5b831a
Alex Avekau <Alex.Avekau@au.abb.com>
2016-07-29 16:22:12 +1000

    prototype

--- app/Util.scala
+++ app/Util.scala
@@ -12,14 +12,17 @@ object Util {
     val config = new SQLiteConfig();
 
     val sqlConnection = DriverManager.getConnection("jdbc:sqlite:db/../db/bookwitz.sqlite", config.toProperties)
-    val resultSet: ResultSet = sqlConnection.prepareStatement("select * from dictionary limit 5 offset 0").executeQuery()
+    val resultSet: ResultSet = sqlConnection.createStatement().executeQuery("select id, word from dictionary order by id")
     val list = new RsIterator(resultSet).map(x => {
       (x.getString("word"), x.getLong("id"))
-    }).toMap
-
+    }).toList
+    var i = 0
     list.foreach(
-      item =>
+      item => {
+        i = i + 1
+        if (i % 100 != 0) print(".") else println(i)
         connect(item._1, item._2, sqlConnection)
+      }
     )
   }
 
@@ -30,36 +33,45 @@ object Util {
         .openConnection(proxy).asInstanceOf[HttpURLConnection]
     connection.setRequestMethod("GET");
     connection.setRequestProperty("User-Agent", "Mozilla/5.0");
-    connection.getResponseCode();
-    var result = Json.parse(scala.io.Source.fromInputStream(connection.getInputStream).mkString)
-    (result \\ "text").foreach(
-      text =>
-        sqlConnection.prepareStatement("insert into dictDefinitions values (NULL, " + wordId + ", " + text + ")").executeUpdate()
-    )
+    if (200 == connection.getResponseCode()) {
+      val result = Json.parse(scala.io.Source.fromInputStream(connection.getInputStream).mkString)
+      (result \\ "text").foreach(
+        text => {
+          val w = text.toString().replaceAll("\\\\\"", "'");
+          sqlConnection.prepareStatement("insert into dictDefinitions values (" + wordId + ", " + w + ", NULL)").executeUpdate()
+        }
+      )
+    }
 
     connection =
       new URL("http://api.wordnik.com:80/v4/word.json/" + word + "/pronunciations?api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5&limit=5")
         .openConnection(proxy).asInstanceOf[HttpURLConnection]
     connection.setRequestMethod("GET");
     connection.setRequestProperty("User-Agent", "Mozilla/5.0");
-    connection.getResponseCode();
-    result = Json.parse(scala.io.Source.fromInputStream(connection.getInputStream).mkString)
-    (result \\ "raw").foreach(
-      text =>
-        sqlConnection.prepareStatement("insert into dictPronunciations values (NULL, " + wordId + ", " + text + ")").executeUpdate()
-    )
+    if (200 == connection.getResponseCode()) {
+      val result = Json.parse(scala.io.Source.fromInputStream(connection.getInputStream).mkString)
+      (result \\ "raw").foreach(
+        text => {
+          val w = text.toString().replaceAll("\\\\\"", "'");
+          sqlConnection.prepareStatement("insert into dictPronunciations values (NULL, " + wordId + ", " + w + ")").executeUpdate()
+        }
+      )
+    }
 
     connection =
       new URL("http://api.wordnik.com:80/v4/word.json/" + word + "/topExample?api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5&limit=5")
         .openConnection(proxy).asInstanceOf[HttpURLConnection]
     connection.setRequestMethod("GET");
     connection.setRequestProperty("User-Agent", "Mozilla/5.0");
-    connection.getResponseCode();
-    result = Json.parse(scala.io.Source.fromInputStream(connection.getInputStream).mkString)
-    (result \\ "text").foreach(
-      text =>
-        sqlConnection.prepareStatement("insert into dictExamples values (" + wordId + ", " + text + ")").executeUpdate()
-    )
+    if (200 == connection.getResponseCode()) {
+      val result = Json.parse(scala.io.Source.fromInputStream(connection.getInputStream).mkString)
+      (result \\ "text").foreach(
+        text => {
+          val w = text.toString().replaceAll("\\\\\"", "'");
+          sqlConnection.prepareStatement("insert into dictExamples values (" + wordId + ", " + w + ")").executeUpdate()
+        }
+      )
+    }
   }
 }
 
